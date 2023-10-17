50b0cbdf133ea18856f70a114976162da23cdc21
uzbcoder0221 <gtermix7@gmail.com>
2023-10-17 22:05:36 +0500

    fix: collected statics v2

--- AzizMuz/settings.py
+++ AzizMuz/settings.py
@@ -108,6 +108,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+STATIC_ROOT = 'statics/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
