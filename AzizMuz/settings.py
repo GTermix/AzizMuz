6094df8c7a95185c3d3b8ed7d90780ae83bdc954
UzBCoder <119860690+UzBCoder0221@users.noreply.github.com>
2024-04-12 20:54:04 +0500

    Update settings.py

--- AzizMuz/settings.py
+++ AzizMuz/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oty2kh^s6x@^v$m!gjhcs+i8h9#+rwx53$)2u4y@453rlm81tj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".onrender.com",
                  "127.0.0.1",
